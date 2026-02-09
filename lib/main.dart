8b005cc000e97f4211a24642c4bc713456f63f43
Rizwan shaikh <rizwansk2306@gmail.com>
2026-02-09 22:57:58 +0530

    final work completed in auth page ui and validation

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // sm
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
